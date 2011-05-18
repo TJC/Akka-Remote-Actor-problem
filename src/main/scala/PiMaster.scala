e4fce74d4af0831943bb6ac0099e725040576f49
Toby Corkindale <tobyc@strategicdata.com.au>
2011-05-18 12:55:39 +1000

    Move actor code to common package.

--- src/main/scala/PiMaster.scala
+++ src/main/scala/PiMaster.scala
@@ -1,10 +1,7 @@
 package tjc.akka
 
-import akka.actor.{Actor, PoisonPill}
+import akka.actor.Actor
 import Actor._
-import akka.routing.{Routing, CyclicIterator}
-import Routing._
-
 import java.util.concurrent.CountDownLatch
 
 import tjc.akka.base._
@@ -12,65 +9,6 @@ import tjc.akka.base._
 object PiMaster extends App {
   calculate(nrOfWorkers = 1, nrOfElements = 100, nrOfMessages = 1)
 
-
-  // ====================
-  // ===== Messages =====
-  // ====================
-  case object Calculate extends PiMessage
-
-  // ==================
-  // ===== Master =====
-  // ==================
-  class Master(
-    nrOfWorkers: Int,
-    nrOfMessages: Int,
-    nrOfElements: Int,
-    latch: CountDownLatch
-  ) extends Actor {
-
-    var pi: Double = _
-    var nrOfResults: Int = _
-    var start: Long = _
-
-    // create the workers
-    val workers = Vector.fill(nrOfWorkers)(
-        remote.actorFor("pi-service", "localhost", 2552)
-    )
-
-    // wrap them with a load-balancing router
-    val router = Routing.loadBalancerActor(CyclicIterator(workers)).start()
-
-    // message handler
-    def receive = {
-      case Calculate =>
-        // schedule work
-        for (i <- 0 until nrOfMessages) router ! Work(i * nrOfElements, nrOfElements)
-
-        // send a PoisonPill to all workers telling them to shut down themselves
-        router ! Broadcast(PoisonPill)
-
-        // send a PoisonPill to the router, telling him to shut himself down
-        router ! PoisonPill
-
-      case Result(value) =>
-        // handle result from the worker
-        pi += value
-        nrOfResults += 1
-        if (nrOfResults == nrOfMessages) self.stop()
-    }
-
-    override def preStart() {
-      start = System.currentTimeMillis
-    }
-
-    override def postStop() {
-      // tell the world that the calculation is complete
-      println(
-        "\n\tPi estimate: \t\t%s\n\tCalculation time: \t%s millis"
-        .format(pi, (System.currentTimeMillis - start)))
-      latch.countDown()
-    }
-  }
 
   // ==================
   // ===== Run it =====
--- src/main/scala/PiSlave.scala
+++ src/main/scala/PiSlave.scala
@@ -9,29 +9,5 @@ object PiSlave extends App {
     remote.start("localhost", 2552)
     remote.register("pi-service", actorOf[Worker])
 
-
-    // ==================
-    // ===== Worker =====
-    // ==================
-    class Worker extends Actor {
-
-        // define the work
-        def calculatePiFor(start: Int, nrOfElements: Int): Double = {
-            var acc = 0.0
-            for (i <- start until (start + nrOfElements))
-                acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
-            acc
-        }
-
-        def receive = {
-            case Work(start, nrOfElements) =>
-                println("slave received Work message")
-                self.reply(
-                    Result(calculatePiFor(start, nrOfElements))
-                )
-            case _ => println("slave received unknown message!")
-        }
-    }
-
 }
 
